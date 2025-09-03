35d08b7035d1c37cd66a5f9917d03a41bba6bcce
Michael Mueller <michael@mdmueller.me>
2025-09-03 15:57:46 -0500

    make constants public

--- Sources/UIKitTools/uikit/Constants.swift
+++ Sources/UIKitTools/uikit/Constants.swift
@@ -5,24 +5,23 @@
 //  Created by Michael Mueller on 9/3/25.
 //
 
-struct Constants {
+public struct Constants {
     
-    static let separatorHeight = 1.0
-    static let lowImpact = 0.2
-    static let mediumImpact = 0.5
-    static let highImpact = 1.0
+    public static let separatorHeight = 1.0
+    public static let lowImpact = 0.2
+    public static let mediumImpact = 0.5
+    public static let highImpact = 1.0
 
-    static let standardCornerRadius = 3.0
-    static let buttonHeight = 56.0
-    static let closeButtonSize: CGFloat = 36.0
+    public static let standardCornerRadius = 3.0
+    public static let buttonHeight = 56.0
+    public static let closeButtonSize: CGFloat = 36.0
 
-    static let extraExtraSmallMargin = 4.0
-    static let extraSmallMargin = 8.0
-    static let smallMargin = 12.0
-    static let standardMargin = 20.0
-    static let largeMargin = 32.0
-    static let extraLargeMargin = 40.0
-    static let extraExtraLargeMargin = 48.0
-
+    public static let extraExtraSmallMargin = 4.0
+    public static let extraSmallMargin = 8.0
+    public static let smallMargin = 12.0
+    public static let standardMargin = 20.0
+    public static let largeMargin = 32.0
+    public static let extraLargeMargin = 40.0
+    public static let extraExtraLargeMargin = 48.0
     
 }
