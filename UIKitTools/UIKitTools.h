23701d906f91666c59345cc618ff6e56dc33a8d9
Michael Mueller <michael@mdmueller.me>
2025-09-03 08:57:31 -0500

    Initial Commit

---
+++ UIKitTools/UIKitTools.h
@@ -0,0 +1,18 @@
+//
+//  UIKitTools.h
+//  UIKitTools
+//
+//  Created by Michael Mueller on 9/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitTools.
+FOUNDATION_EXPORT double UIKitToolsVersionNumber;
+
+//! Project version string for UIKitTools.
+FOUNDATION_EXPORT const unsigned char UIKitToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitTools/PublicHeader.h>
+
+
