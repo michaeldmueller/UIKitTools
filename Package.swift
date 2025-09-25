f89a3cbf1e767c8d7e17fd79441fffa1e56b7388
Michael Mueller <michael@mdmueller.me>
2025-09-25 16:31:14 -0500

    add dependency

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             targets: ["UIKitTools"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "2.1.1")
+    ],
     targets: [
         .target(
             name: "UIKitTools",
